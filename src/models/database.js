d6f7b0737b2d6d569cdcb33f40875341c8524a32
ixphinx <48696004+ixphinx@users.noreply.github.com>
2020-03-26 22:28:45 -0300

    Update

--- src/models/database.js
+++ src/models/database.js
@@ -3,85 +3,8 @@ const {Schema} = mongoose;
 
 const Data = new Schema({
     
-    /*-----index-----*/
-    ipart:String,
-    iparc:String,
-    
-    /*----- Noticias---- */
-    noticias:String,
-
-    /*------ Comentarios -----*/
-    comhead: String,
-    combody: String,
-    comhead1: String,
-    combody1: String,
-
-    /*---- Jugadores ----- */
-    jnombre: String,
-    jimg: String,
-    japellido: String,
-    jcedula: String,
-    jgenero: String,
-    jgoles: String,
-    jtarjetasam: String,
-    jtarjestasroj: String,
-    jamonestaciones: String,
-    jequipo: String,
-    jdivision: String,
-
-    /*------ Equipos ----- */
-    enombre: String,
-    eescudo: String,
-    egenero: String,
-    edivision: String,
-    edelegado: String,
-    edelcedula: String,
-    epuntos: String,
-    epartidosj: String,
-    epartidosg: String,
-    epartidosempatados: String,
-    epartidosperdidos: String,
-    egolesmas: String,
-    egolesmenos: String,
-    esaldo: String,
-    efairplay: String,
-
-    /*------- Torneos ------*/
-    tfecha: String,
-    tnombre: String,
-    thora: String,
-    tea: String,
-    teb: String,
-    tinfo: String,
-    testado: String,
-
-    /*------- Partidos ------*/
-    pfecha: String,
-    pequipoa: String,
-    pequipob: String,
-    peamarcador: Number,
-    pebmarcador: Number,
-    peagoles: String,
-    pebgoles: String,
-    pamarillasa: String,
-    pamarillasb: String,
-    projasa: String,
-    projasb: String,
-    pfpa: String,
-    pfpb: String,
-    parbitro: String,
-    pobservaciones: String,
-
-    /*---- Seguridad------*/
-    paneladmin: String,
-    delegado:String,
-
-    /*----- Mensaje ----*/
-    msgemail: String,
-    msgbody: String,
-
-    /*---- Kaygua ----*/
     msgname: String,
+    msglastname: String,
     msgmail: String,
     msgtel: String,
     msgtext: String
